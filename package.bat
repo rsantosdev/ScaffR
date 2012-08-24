9fa8edd04cf1eaa7306a4726801f85ca9f2a7ecd
wcpro <rodmjay@wcpro.com>
2012-08-24 04:48:05 -0600

    Build all nuget packages at once

---
+++ package.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: Set repository info
+set NUGET="nuget/nuget.exe"
+set key={your-api-key}
+set url={nuget-gallery-url}
+set path=c:\development\localnuget
+
+attrib -R %NUGET%
+cmd /c %NUGET% update -self
+
+IF not exist %path% mkdir %path%
+
+for /R %%i in (*.nuspec) do (
+
+ SET NUSPEC=%%i
+ echo %%i
+
+ %NUGET% pack %%i -o %path% -Version 1.1.1
+
+)
