38f3400544db69d89fdb9d23f6996d21c8db70e0
Rod Johnson <rod@scaffr.com>
2012-09-20 02:15:21 -0600

    Better configuration, more modular

---
+++ src/ScaffR.Environment/content/CodeTemplates/Scaffolders/ScaffR.Environment/ScaffR.Environment.ps1
@@ -0,0 +1,34 @@
+﻿[T4Scaffolding.Scaffolder()][CmdletBinding()]
+param(        
+    [string]$Project,
+	[string]$CodeLanguage,
+	[string[]]$TemplateFolders,
+	[switch]$Force = $false
+)
+
+ $templates = 
+ 	@("Environment","DevLevel")
+
+foreach ($tml in $templates){
+	$outputPath = "Common\Environment\$tml"
+	add-template $coreProjectName $outputPath $tml -Force:$Force $TemplateFolders
+}
+
+ $templates = 
+ 	@("EnvironmentElement")
+
+foreach ($tml in $templates){
+	$outputPath = "Configuration\Environment\$tml"
+	add-template $coreProjectName $outputPath $tml -Force:$Force $TemplateFolders
+}
+
+ $templates = 
+ 	@("CoreSection.Environment")
+
+foreach ($tml in $templates){
+	$outputPath = "Configuration\$tml"
+	add-template $coreProjectName $outputPath $tml -Force:$Force $TemplateFolders
+}
+
+
+
